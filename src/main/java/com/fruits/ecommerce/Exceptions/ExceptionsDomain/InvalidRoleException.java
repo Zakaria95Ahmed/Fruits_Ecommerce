e77a39a7834269746d3b9bd16152043b88a462c3
Zakaria95Ahmed <zakariayahmed@gmail.com>
2024-10-06 21:08:27 +0300

    delete all project files

--- src/main/java/com/fruits/ecommerce/Exceptions/ExceptionsDomain/InvalidRoleException.java
+++
@@ -1,9 +0,0 @@
-package com.fruits.ecommerce.Exceptions.ExceptionsDomain;
-
-import com.fruits.ecommerce.Exceptions.Global.UserServiceException;
-
-public class InvalidRoleException extends UserServiceException {
-    public InvalidRoleException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/fruits/ecommerce/Models/Enum/RoleType.java
+++
@@ -1,7 +0,0 @@
-package com.fruits.ecommerce.Models.Enum;
-
-public enum RoleType {
-    VISITOR,
-    CLIENT,
-    ADMIN
-}
